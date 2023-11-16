80cc192f42c6580e573b218c0ac1252cbe2cc496
Brandon <banorton1@gmail.com>
2023-11-15 20:40:34 -0500

    Made dictionary from ipa to naru

--- naru.py
+++ naru.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from matplotlib import pyplot as plt
 import random as rand
+import eng_to_ipa as ipa
 
 
 def load_images_from_folder(folder):
@@ -15,20 +16,38 @@ def load_images_from_folder(folder):
     return images
 
 
-def en2naru(txt):
-    assert isinstance(txt, str)
+class naru:
+    naru_vowels = load_images_from_folder(os.getcwd() + "/script2/vowels")
+    naru_consonants = load_images_from_folder(os.getcwd() + "/script2/consonants")
 
-    imgs = load_images_from_folder(os.getcwd() + "/imgs/chars")
-    img = rand.choice(imgs)
-    for char in txt[1:]:
-        img = np.concatenate((img, rand.choice(imgs)), axis=1)
+    # fmt: off
+    ipa_vowels = ['ə', 'ɪ', 'i', 'ɛ', 'æ', 'u', 'e', 'ɑ', 'ɔ', 'o', 'a', 'ʊ']
+    ipa_consonants = ['n', 'r', 't', 's', 'd', 'l', 'k', 'ð', 'm', 'z', 'p', 'v', 'w', 'b', 'f', 'h', 'ŋ', 'ʃ', 'j', 'g', 'ʤ', 'ʧ', 'θ', 'ʒ']
+    # fmt: on
 
-    return img
+    @staticmethod
+    def en2naru(txt):
+        return None
+
+    @staticmethod
+    def dictionary():
+        vlen = min(len(naru.ipa_vowels), len(naru.naru_vowels))
+        vzip = list(zip(naru.ipa_vowels[:vlen], naru.naru_vowels[:vlen]))
+
+        clen = min(len(naru.ipa_consonants), len(naru.naru_consonants))
+        czip = list(zip(naru.ipa_consonants[:clen], naru.naru_consonants[:clen]))
+
+        return [entry for entry in vzip] + [entry for entry in czip]
 
 
 if __name__ == "__main__":
-    naru_name = en2naru("brandon")
+    print(naru.dictionary())
+    # naru_name = en2naru("brandon")
+    # plt.imshow(naru_name, cmap="gray")
+    # plt.axis("off")
+    # plt.show()
 
-    plt.imshow(naru_name, cmap="gray")
-    plt.axis("off")
-    plt.show()
+    # print(len(naru.naru_vowels))
+    # print(len(naru.naru_consonants))
+    # print(len(naru.ipa_vowels))
+    # print(len(naru.ipa_consonants))
