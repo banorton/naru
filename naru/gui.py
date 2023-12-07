d87c36282b114c1211b0485df65b37a7ff89e764
Brandon <banorton1@gmail.com>
2023-12-07 12:33:37 -0500

    fixed alignment problems

--- naru/gui.py
+++ naru/gui.py
@@ -20,16 +20,28 @@ class Gui:
 
         # Top Frame
         translate_eng = ttk.Frame(root)
-        translate_eng_label = ttk.Label(translate_eng, text="English")
-        translate_eng_label.pack(side="left", pady=10)
+        translate_eng_label = ttk.Label(
+            translate_eng,
+            text="English",
+            font="Helvetica 18 bold",
+            width=8,
+            anchor="e",
+        )
+        translate_eng_label.pack(side="left", padx=5)
         translate_eng_text = tk.Text(translate_eng)
-        translate_eng_text.pack(side="right")
+        translate_eng_text.pack(side="right", fill="both", pady=5)
         translate_eng.grid(row=0, column=0, padx=10)
 
         # Bottom Frame
         translate_naru = ttk.Frame(root)
-        translate_naru_label = ttk.Label(translate_naru, text="Naru")
-        translate_naru_label.pack(side="left", pady=10)
+        translate_naru_label = ttk.Label(
+            translate_naru,
+            text="Naru",
+            font="Helvetica 18 bold",
+            width=8,
+            anchor="e",
+        )
+        translate_naru_label.pack(side="left", padx=5)
         translate_naru_text = tk.Text(translate_naru)
-        translate_naru_text.pack(side="right", fill="both")
+        translate_naru_text.pack(side="right", fill="both", pady=5)
         translate_naru.grid(row=1, column=0, padx=10)
