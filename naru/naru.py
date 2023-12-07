d24b852324aafda8dc1b65190b978e145226f5d1
Brandon <banorton1@gmail.com>
2023-12-07 16:29:10 -0500

    removed deticated gui file. gui wip

--- naru/naru.py
+++ naru/naru.py
@@ -1,9 +1,12 @@
 import numpy as np
 import os
+from os import path
 from .funcs import load_images_from_folder
 from matplotlib import pyplot as plt
 import eng_to_ipa as ipa
-from .gui import Gui
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 class Naru:
@@ -53,8 +56,10 @@ class Naru:
             ln[-1] += close
 
             # Add a space between words.
-            ln.append(np.ones((27, 8)) * 255)
+            if len(ln) < syl_lim:
+                ln.append(np.ones((27, 8)) * 255)
         Naru._addendspace(lns[-1], syl_lim)
+        print(lns[-1].shape)
         for i in range(len(lns)):
             lns[i] = np.concatenate(lns[i], axis=1)
             lnspc = np.ones((8, lns[0].shape[1])) * 255
@@ -95,17 +100,74 @@ class Naru:
         return dict([entry for entry in vzip] + [entry for entry in czip])
 
     @staticmethod
-    def plot(words, naruwords, title=True):
-        arrsz = (len(words) // 2, 2)
-        for i in range(len(naruwords)):
-            plt.subplot(arrsz[0], arrsz[1], i + 1)
-            plt.imshow(naruwords[i], cmap="gray")
-            plt.axis("off")
-            if title:
-                plt.title(words[i])
-        plt.show()
+    def plot(naru_words):
+        fig = plt.figure()
+        plt.imshow(naru_words, cmap="gray")
+        plt.axis("off")
+        return fig
 
+    @staticmethod
     def mkgui():
         gui = Gui()
         gui.root.mainloop()
         return gui
+
+
+class Gui:
+    def __init__(self, parent=None):
+        self.parent = parent
+        root = tk.Tk()
+        self.root = root
+        root.geometry("800x600")
+        root.resizable(width=False, height=False)
+        root.title("Translator")
+        root.rowconfigure(0, weight=10)
+        root.rowconfigure(1, weight=1)
+        style = ttk.Style(root)
+        root.tk.call("source", path.join(path.dirname(__file__), "forest-dark.tcl"))
+        style.theme_use("forest-dark")
+
+        # Top Frame
+        translate_eng = ttk.Frame(root)
+        info_frame = ttk.Frame(translate_eng)
+        translate_eng_label = ttk.Label(
+            info_frame,
+            text="English",
+            font="Helvetica 18 bold",
+            width=8,
+            anchor="e",
+        )
+        translate_eng_label.pack(side="top", padx=5)
+        translate_btn = ttk.Button(
+            info_frame, text="Translate", command=self.translate_and_plot
+        )
+        translate_btn.pack(side="bottom", pady=5)
+        info_frame.pack(side="left")
+        translate_eng_text = tk.Text(translate_eng)
+        translate_eng_text.pack(side="right", fill="both", pady=5)
+        translate_eng_text.focus()
+        translate_eng.grid(row=0, column=0, padx=5)
+        self.entry = translate_eng_text
+
+        # Bottom Frame
+        translate_naru = ttk.Frame(root)
+        translate_naru_label = ttk.Label(
+            translate_naru,
+            text="Naru",
+            font="Helvetica 18 bold",
+            width=8,
+            anchor="e",
+        )
+        translate_naru_label.pack(side="left", padx=5)
+        naru_text_frame = ttk.Frame(translate_naru)
+        self.naru_frame = naru_text_frame
+        naru_text_frame.pack(side="right", padx=5)
+        translate_naru.grid(row=1, column=0, padx=5)
+
+    def translate_and_plot(self):
+        txt = self.entry.get("1.0", "end-1c")
+        naru_txt = Naru.translate(txt)
+        gui_fig = Naru.plot(naru_txt)
+        naru_plt = FigureCanvasTkAgg(gui_fig, master=self.naru_frame)
+        naru_plt.draw()
+        naru_plt.get_tk_widget().pack()
